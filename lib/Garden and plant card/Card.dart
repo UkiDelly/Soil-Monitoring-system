953bc9854d399d0c2598e4207c60f6bea9e2bdb6
daehyeon1999 <daehyeon1999@naver.com>
2022-01-23 16:01:07 +0800

    Add Garden page
    add the page for the garden

--- lib/Garden and plant card/Card.dart
+++ lib/Garden and plant card/Card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:thesis/Gardens/Garden%20Page.dart';
 
 class GardenAndPlantCard extends StatefulWidget {
   int? gardenOrPlant;
@@ -15,13 +16,26 @@ class _GardenAndPlantCardState extends State<GardenAndPlantCard> {
       padding: const EdgeInsets.all(8.0),
 
       // Card
-      child: Card(
-        elevation: 5,
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(25)),
-        color:
-            widget.gardenOrPlant == 1 ? const Color(0xff669D6B) : Colors.white,
-        child: Container(
-          width: 200,
+      child: GestureDetector(
+        onTap: () {
+          if (widget.gardenOrPlant == 1) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => Garden()),
+            );
+          } else {}
+        },
+        child: Card(
+          elevation: 5,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(25)),
+          color: widget.gardenOrPlant == 1
+              ? const Color(0xff669D6B)
+              : Colors.white,
+          child: Container(
+            width: 200,
+            //child: ,
+          ),
         ),
       ),
     );
