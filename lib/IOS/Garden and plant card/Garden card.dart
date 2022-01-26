bf8df89cffdecb9f43008785c9f4ecd7a522c1d0
Dae Hyeon <daehyeon1999@naver.com>
2022-01-26 11:38:24 +0800

    Add Soil History Page and the appbar

--- lib/IOS/Garden and plant card/Garden card.dart
+++ lib/IOS/Garden and plant card/Garden card.dart
@@ -49,52 +49,55 @@ class _Card extends StatefulWidget {
 class __CardState extends State<_Card> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: SizedBox(
-        width: 200,
-        child: Stack(
-          children: [
-            // Name of the garden
-            Positioned(
-                top: 15,
-                left: 17,
-                width: 125,
-                child: Text("#1",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                        fontFamily: "Readex Pro",
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold))),
-            Positioned(
-              top: 8,
-              right: 10,
-              child: SizedBox(
-                  width: 30,
-                  height: 30,
-                  // if there is laking, use warning
-                  child: SvgPicture.asset(
-                    "assets/check.svg",
-                    color: Colors.white,
-                  )),
-            ),
+    return SizedBox(
+      width: 200,
+      child: Stack(
+        children: [
+          // Name of the garden
+          Positioned(
+              top: 15,
+              left: 17,
+              width: 125,
+              child: Text("#1",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontFamily: "Readex Pro",
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold))),
+          Positioned(
+            top: 8,
+            right: 10,
+            child: SizedBox(
+                width: 30,
+                height: 30,
+                // if there is laking, use warning
+                child: SvgPicture.asset(
+                  "assets/check.svg",
+                  color: Colors.white,
+                )),
+          ),
 
-            // comment of the garden
-            const Positioned(
-                width: 100,
-                right: 10,
-                top: 70,
-                child: Text(
-                    "Summary of the status of the soil and comment will be display here!~~"))
-          ],
-        ),
+          // comment of the garden
+          const Positioned(
+              width: 100,
+              right: 10,
+              top: 70,
+              child: Text(
+                  "Summary of the status of the soil and comment will be display here!~~"))
+        ],
       ),
     );
   }
 }
 
-class AddGarden extends StatelessWidget {
+class AddGarden extends StatefulWidget {
   const AddGarden({Key? key}) : super(key: key);
 
+  @override
+  State<AddGarden> createState() => _AddGardenState();
+}
+
+class _AddGardenState extends State<AddGarden> {
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -102,6 +105,18 @@ class AddGarden extends StatelessWidget {
 
         // Card
         child: OpenContainer(
+            transitionDuration: const Duration(seconds: 1),
+            closedElevation: 5,
+            // When the Container is closed
+            onClosed: (data) {
+              setState(() {});
+            },
+            openColor: const Color(0xfffefefe),
+            //Color when the Container is closed
+            closedColor: const Color(0xfffefefe),
+            //Shape of the close Container
+            closedShape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(25)),
             closedBuilder: (_, openBuilder) => _AddGardenCard(),
             openBuilder: (_, closeBuilder) => NewGardenCard()));
   }
@@ -112,6 +127,22 @@ class _AddGardenCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SizedBox(
+      width: 200,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: const [
+          Icon(
+            Icons.add,
+            color: Colors.grey,
+            size: 70,
+          ),
+          Text(
+            "Add New Garden",
+            style: TextStyle(fontSize: 15),
+          )
+        ],
+      ),
+    );
   }
 }
--- lib/IOS/Gardens/New Garden.dart
+++ lib/IOS/Gardens/New Garden.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class NewGardenCard extends StatelessWidget {
   const NewGardenCard({Key? key}) : super(key: key);
@@ -6,6 +7,28 @@ class NewGardenCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // make a new garden
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: const Color(0xff669D6B),
+      appBar: AppBar(
+        backgroundColor: const Color(0xff669D6B),
+        elevation: 0,
+        leading: IconButton(
+          icon: SvgPicture.asset(
+            'assets/back.svg',
+            width: 15,
+            color: Colors.white,
+          ),
+          onPressed: () => Navigator.pop(context),
+        ),
+        centerTitle: true,
+        title: const Text(
+          "Soil Status History",
+          style: TextStyle(color: Colors.black),
+        ),
+      ),
+      body: Container(
+          //color: Colors.grey,
+          ),
+    );
   }
 }
