62b1d149f2204130ea062b3c3a1e17e32a2f39a0
Dae Hyeon <daehyeon1999@naver.com>
2022-01-30 16:58:55 +0800

    add Name textfield on new garden page

--- lib/IOS/Gardens/new_garden_page.dart
+++ lib/IOS/Gardens/new_garden_page.dart
@@ -1,50 +1,115 @@
 import 'package:flutter/material.dart';
 
-class AddNewGarden extends StatelessWidget {
-  const AddNewGarden({Key? key}) : super(key: key);
+class AddNewGarden extends StatefulWidget {
+  AddNewGarden({Key? key}) : super(key: key);
 
   @override
+  State<AddNewGarden> createState() => _AddNewGardenState();
+}
+
+class _AddNewGardenState extends State<AddNewGarden> {
+  void initState() {
+    super.initState();
+  }
+
+  //text controller
+  var nameControl = TextEditingController();
+
   Widget build(BuildContext context) {
-    return Scaffold(
-      // App bar
-      appBar: AppBar(
+    @override
+    var name;
+
+    return GestureDetector(
+      //unfocuse eveything if tap the background
+      onTap: () {
+        FocusScope.of(context).requestFocus(new FocusNode());
+      },
+      child: Scaffold(
+        // App bar
+        appBar: AppBar(
+          backgroundColor: const Color(0xff669D6B),
+
+          // Cancel button
+          leading: TextButton(
+            //disable splash
+            style: ButtonStyle(
+              overlayColor: MaterialStateProperty.all(Colors.transparent),
+            ),
+            child: const Text(
+              "Cancel",
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold),
+            ),
+            onPressed: () => Navigator.pop(context),
+          ),
+          leadingWidth: 90,
+
+          elevation: 0,
+          actions: [
+            // Add button
+            TextButton(
+                //disable splash
+                style: ButtonStyle(
+                  overlayColor: MaterialStateProperty.all(Colors.transparent),
+                ),
+                onPressed: () {},
+                child: const Text(
+                  "Add",
+                  style: TextStyle(
+                      color: Color(0xfffefefe),
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold),
+                ))
+          ],
+        ),
+
         backgroundColor: const Color(0xff669D6B),
 
-        // Cancel button
-        leading: TextButton(
-          //disable splash
-          style: ButtonStyle(
-            overlayColor: MaterialStateProperty.all(Colors.transparent),
-          ),
-          child: const Text(
-            "Cancel",
-            style: TextStyle(
-                color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),
-          ),
-          onPressed: () => Navigator.pop(context),
-        ),
-        leadingWidth: 90,
-
-        elevation: 0,
-        actions: [
-          // Add button
-          TextButton(
-              //disable splash
-              style: ButtonStyle(
-                overlayColor: MaterialStateProperty.all(Colors.transparent),
+        body: SizedBox(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // Name
+              const Padding(
+                padding: EdgeInsets.only(left: 45, bottom: 10),
+                child: Text(
+                  "Name",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
               ),
-              onPressed: () {},
-              child: const Text(
-                "Add",
-                style: TextStyle(
-                    color: Color(0xfffefefe),
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold),
-              ))
-        ],
-      ),
 
-      backgroundColor: const Color(0xff669D6B),
+              // Name text field
+              Padding(
+                padding: const EdgeInsets.only(left: 30, right: 30),
+                child: TextField(
+                  style: const TextStyle(fontSize: 20),
+                  controller: nameControl,
+                  decoration: const InputDecoration(
+                      filled: true,
+                      fillColor: Color(0xfffefefe),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.transparent),
+                        borderRadius: BorderRadius.all(Radius.circular(20)),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.transparent),
+                        borderRadius: BorderRadius.all(Radius.circular(20)),
+                      )),
+                ),
+              ),
+
+              // Avail sensor
+
+              const SizedBox(
+                height: 500,
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
